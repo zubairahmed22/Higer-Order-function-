3073364e0e5ea2ac9640acdfa0af1b33d36f26f5
unknown <zubairwastibaloch@gmail.com>
2021-08-26 07:31:15 -0700

    higer order function prictice

---
+++ index.js
@@ -0,0 +1,25 @@
+let companies = [
+    {
+        name:"google",
+        Employeer:3000,
+        HeadOffice: "new Yark"
+    },
+    {
+        name:"Azamon",
+        Employeer:5000,
+        HeadOffice: "washention Dc"
+    },
+    {
+        name:"Alibaba",
+        Employeer:4000,
+        HeadOffice: "Beging"
+    }
+]
+
+// for(let i = 0; i < companies.length; i++){
+//  console.log(companies[i].name)
+// }
+// companies.forEach(function(company){
+//     console.log(company.HeadOffice)
+
+// })
